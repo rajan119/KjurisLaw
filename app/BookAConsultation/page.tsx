c506335659c4aa8a6e95108d0395505f840aa0bc
rajan119 <mauryarajan119@gmail.com>
2026-07-30 21:13:39 +0530

    BookAC page update

---
+++ app/BookAConsultation/page.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "@/components/navbar";
+import Footer from "@/components/Footer";
+
+export default function Page() {
+  return (
+    <>
+      <Navbar />
+      <h1>In Development</h1>
+      <Footer />
+    </>
+  );
+}
